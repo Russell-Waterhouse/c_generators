c620feb3ad673323ad13372f284858058db32082
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2025-12-07 14:29:57 -0700

    Create strings.h

---
+++ strings.h
@@ -0,0 +1,30 @@
+typedef struct String {
+  size_t size;
+  size_t memsize;
+  char* str;
+} String;
+
+typedef struct DynStringArr {
+  size_t memsize;
+  size_t size;
+  String* arr;
+} DynStringArr;
+
+typedef struct SplitResultOption {
+  Result status;
+  u32 num_strs;
+  DynStringArr* strs;
+} SplitResultOption;
+
+typedef struct SplitResult {
+  u32 num_strs;
+  DynStringArr* strs;
+} SplitResult;
+
+String* cstr_to_str_or_die(char* cstr, u32 size);
+String* u64_to_str_or_die(u64 v);
+Result strip_in_place(String* s);
+void free_str_or_die(String* s);
+SplitResult split_str_or_die(String* s, char split_char);
+SplitResultOption split_str(String* s, char split_char);
+String* cstr_to_str_or_die(char* cstr, u32 size) {
